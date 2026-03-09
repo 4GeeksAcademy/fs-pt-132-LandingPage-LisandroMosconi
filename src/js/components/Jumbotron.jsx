2dd0cb4c66e5625e7ad0fa20b42d7707422e755d
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-03-09 20:27:30 +0000

    feat: basic bootstrap for the Jumbotron

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return <div className="container sm-8 md-6 lg-4 text-start">
+        <h2>Jumbotron</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolore eos suscipit? 
+            Nesciunt inventore ea cumque! Minima facere fugit necessitatibus nihil reprehenderit! 
+            Accusamus consequatur quas quia vel doloribus sunt rerum.</p>
+        <button>Jumboclick</button>
+    </div>
+}
+
+export default Jumbotron
