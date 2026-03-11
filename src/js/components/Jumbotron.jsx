ec0743643ddf2e36ef2a5fd72989c18f04c42a5a
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-03-11 20:59:59 +0000

    change: changed text of the content

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -2,19 +2,23 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div className="p-5 mb-4 bg-body-secondary rounded-3">
+    <div className="p-5 mb-4 jumbotronColor rounded-3">
       <div className="container py-5 text-start">
-        <h1 className="display-5 fw-bold">Jumbotron</h1>
+        <h1 className="display-5 fw-bold">Full character list and releases</h1>
 
-        <p className="col-md-8 fs-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolore eos suscipit?
-          Nesciunt inventore ea cumque! Minima facere fugit necessitatibus nihil reprehenderit!
-          Accusamus consequatur quas quia vel doloribus sunt rerum.
+        <p className="col-sm-12 col-md-8 col-lg-6 fs-5">
+          Here is a list with small details about the characters, their role and element.
+          Feel free to check on the main website by clicking the button below.
         </p>
 
-        <button className="btn btn-primary btn-lg">
-          Jumboclick
-        </button>
+        <a
+          href="https://endfield.gryphline.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn arknightsButton btn-lg"
+        >
+          Arknights Endfield Web
+        </a>
 
       </div>
     </div>
@@ -22,3 +26,4 @@ const Jumbotron = () => {
 };
 
 export default Jumbotron
+
