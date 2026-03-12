ad45fcc2e859c1c3f4d441305d285ca3a8caa766
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-03-12 22:16:37 +0000

    change: modified content to match the props

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({url, title, release, birth, role}) => {
 
     return (
         <div className="col-sm-12 col-md-6 col-lg-4">
             <div className="card mb-3">
-                <img src="..." className="card-img-top" alt="..." />
+                <div className="ratio ratio-1x1">
+                <img src={url} className="card-img-top" alt={title} />
+                </div>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{release} | {role} </p>
+                    <p className="card-text"><small class="text-body-secondary"> {birth}</small></p>
                 </div>
             </div>
         </div>
